ff34ca3a049a8375874ae4b3d87d3649d5fec08c
Monique Finley <altimagewritingco@gmail.com>
2022-12-05 00:20:56 -0500

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 function compute(){
-    // get the numbers and compute the values
+    // get numbers and calculate values
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    
-    // result interest text
-    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"</mark>%\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"   
-   
-    // validate principal and alert user to enter positive number
-    if (principal <= 0 || isNaN(principal))
-    {
-        alert("Enter a valid number");
-        document.getElementById("principal").focus();
-        return
-
-    }
-
+    var interest = principal * years * rate/100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var result = document.getElementById("result");
+    // validate interest rates by number of years 
+    validate()
+    result.innerHTML = `If you deposit <mark>${principal}</mark> <br > at an interest rate of <mark>${rate}%</mark>. <br> You will receive an amount of <mark>${interest}</mark>, <br> in the year <mark>${year}</mark>`
 }
-// update rate value
+// rate slider
 function updateRate() {
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    var rateValue = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=`${rateValue} %`;
+}
+
+// alert user to enter positive number
+function validate() {
+    var prInput = document.getElementById("principal").value;
+    if(prInput <= 0) {
+        alert('Enter a positive number')
+        document.getElementById("pricipal").value = '';
     }
-
-
-        
+}
