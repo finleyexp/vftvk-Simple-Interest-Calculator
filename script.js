eb218e7aaf8b87d4bdd510ba760d1e0dff53602f
Monique Finley <altimagewritingco@gmail.com>
2022-12-04 23:08:00 -0500

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute(){
+    // get the numbers and compute the values
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
     
+    // result interest text
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"</mark>%\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"   
+   
+    // validate principal and alert user to enter positive number
+    if (principal <= 0 || isNaN(principal))
+    {
+        alert("Enter a valid number");
+        document.getElementById("principal").focus();
+        return
+
+    }
+
 }
+// update rate value
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+    }
+
+
         
